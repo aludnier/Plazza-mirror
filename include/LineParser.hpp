8cc9a458a706fd2b972d06eda58f709a62605471
Ayrton LUDGER-CROSNIER <ayrton.ludger-crosnier@epitech.eu>
2026-05-18 13:55:32 +0200

    feat: add line parser for shell

---
+++ include/LineParser.hpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2026
+** lineParser
+** File description:
+** 
+*/
+
+
+#ifndef LINEPARSER
+    #define LINEPARSER
+    #include <string>
+    #include <sstream>
+    #include <vector>
+
+
+
+
+class LineParser
+{
+private:
+    std::string _line;
+    std::vector<std::string> _words;
+    char _delim;
+
+    void Update_words(char delim);
+public:
+    LineParser();
+    ~LineParser();
+
+    class ParserError : public std::exception {
+        private:
+            std::string _msg;
+        public:
+            ParserError(std::string msg): _msg("[LineParse Error] : " + msg) {};
+            const char *what() const noexcept override {
+                return _msg.data();
+            };
+    };
+
+    void ParseLine(std::string newline, char delim = ' ');
+    std::size_t getNbWord();
+    std::vector<std::string>& getWords();
+    std::string getLine();
+    void cleanChars(std::string charToRemove);
+    void replaceWord(std::size_t index, std::string newWord);
+    std::string concatWords(std::string delim = " ");
+    void removeWord(std::size_t index);
+};
+
+
+#endif
