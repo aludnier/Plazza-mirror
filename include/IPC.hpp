bb1d795715b65b501e7be2d5545b05158184186c
Alen Sarsen <alen.sarsen@epitech.eu>
2026-05-20 23:43:34 +0200

    feat: added an IPC that sends and receives the messages

---
+++ include/IPC.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2026
+** plazza-mirror
+** File description:
+** IPC
+*/
+
+#ifndef IPC_HPP_
+    #define IPC_HPP_
+    #include "Order.hpp"
+    #include <string>
+    #include <sys/msg.h>
+    #include <sys/ipc.h>
+    #include <string.h>
+    #define BUFFER_SIZE 512
+struct Buffer
+{
+    long type;
+    char text[BUFFER_SIZE];
+};
+
+class IPC
+{
+    private:
+        key_t _key;
+        int _id;
+
+    public:
+        IPC();
+        ~IPC();
+
+        IPC &operator<<(const std::string &msg);
+        IPC &operator>>(std::string &msg);
+
+        class IPCError : public std::exception {
+            private:
+                std::string _msg;
+            public:
+                IPCError(std::string msg): _msg("[IPC Error] : " + msg) {};
+                const char *what() const noexcept override {
+                    return _msg.data();
+                };
+        };
+};
+
+#endif /* !IPC_HPP_ */
---
+++ src/IPC.cpp
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2026
+** plazza-mirror
+** File description:
+** IPC
+*/
+
+#include "IPC.hpp"
+
+IPC::IPC()
+{
+    _key = std::rand();
+    _id = msgget(_key, 0666 | IPC_CREAT);
+    if (_id == -1)
+        throw IPCError("msgget failed.");
+}
+
+IPC::~IPC()
+{
+    msgctl(_id, IPC_RMID, nullptr);
+}
+
+IPC &IPC::operator<<(const std::string &msg)
+{
+    Buffer buff;
+
+    buff.type = 1;
+    strncpy(buff.text, msg.c_str(), msg.size());
+    if (msgsnd(_id, &buff, sizeof(buff.text), IPC_NOWAIT) == -1)
+        throw IPCError("msgsnd failed.");
+    return *this;
+}
+
+IPC &IPC::operator>>(std::string &msg)
+{
+    Buffer buff;
+    if (msgrcv(_id, &buff, sizeof(buff.text), 1, IPC_NOWAIT) == -1)
+        throw IPCError("msgrcv failed.");
+    msg = buff.text;
+    return *this;
+}
