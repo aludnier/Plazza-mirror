55b7b0728ffbdc5b38acc72a6b6814c381cd6b2d
Alen Sarsen <alen.sarsen@epitech.eu>
2026-05-25 13:25:36 +0200

    feat: added Ingredient enum and recipe to the PizzaOrderenum

--- include/Order.hpp
+++ include/Order.hpp
@@ -8,9 +8,24 @@
 #pragma once
 #include <iostream>
 
-namespace Plazza {
-    
-    enum PizzaType {
+
+namespace Plazza
+{
+
+enum Ingredient
+{
+    DOUGH,
+    TOMATO,
+    GRUYERE,
+    HAM,
+    MUSHROOMS,
+    STEAK,
+    EGGPLANT,
+    GOAT_CHEESE,
+    CHIEF_LOVE
+};
+
+enum PizzaType {
     Regina = 1,
     Margarita = 2,
     Americana = 4,
@@ -29,6 +44,7 @@ struct PizzaOrder {
     PizzaType _type;
     PizzaSize _size;
     std::size_t _cookTime;
+    std::list<Ingredient> _recipe;
 };
 
 std::ostream &operator<<(std::ostream &s ,const PizzaOrder &order);
@@ -36,25 +52,25 @@ std::ostream &operator<<(std::ostream &s ,const PizzaOrder &order);
 struct ReginaOrder : public PizzaOrder
 {
     ReginaOrder(PizzaSize size) :
-        PizzaOrder({Regina, size, 2}) {};
+        PizzaOrder({Regina, size, 2, {DOUGH, TOMATO, GRUYERE, HAM, MUSHROOMS}}) {};
 };
 
 struct MargaritaOrder : public PizzaOrder
 {
     MargaritaOrder(PizzaSize size) :
-        PizzaOrder({Margarita, size, 1}) {};
+        PizzaOrder({Margarita, size, 1, {DOUGH, TOMATO, GRUYERE}}) {};
 };
 
 struct AmericanaOrder : public PizzaOrder
 {
     AmericanaOrder(PizzaSize size) :
-        PizzaOrder({Americana, size, 2}) {};
+        PizzaOrder({Americana, size, 2, {DOUGH, TOMATO, GRUYERE, STEAK}}) {};
 };
 
 struct FantasiaOrder : public PizzaOrder
 {
     FantasiaOrder(PizzaSize size) :
-        PizzaOrder({Fantasia, size, 4}) {};
+        PizzaOrder({Fantasia, size, 4, {DOUGH, TOMATO, EGGPLANT, GOAT_CHEESE, CHIEF_LOVE}}) {};
 };
 
 
